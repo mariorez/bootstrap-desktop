aba000043964375e5f9b5712f5b4b054a402e3fd
Mario Rezende <mariorez@gmail.com>
2022-07-25 08:30:59 -0300

    build: upgrade Fleks version 1.5-JVM

--- core/src/main/kotlin/util.kt
+++ core/src/main/kotlin/util.kt
@@ -3,10 +3,16 @@ import com.badlogic.gdx.graphics.Color
 import com.badlogic.gdx.graphics.Texture
 import com.badlogic.gdx.graphics.g2d.BitmapFont
 import com.badlogic.gdx.graphics.g2d.NinePatch
+import com.badlogic.gdx.graphics.g2d.Sprite
+import com.badlogic.gdx.math.Vector2
 import com.badlogic.gdx.scenes.scene2d.ui.Button
 import com.badlogic.gdx.scenes.scene2d.ui.TextButton
 import com.badlogic.gdx.scenes.scene2d.utils.NinePatchDrawable
 import com.badlogic.gdx.scenes.scene2d.utils.TextureRegionDrawable
+
+fun Sprite.getCenterX(): Float = this.originX + this.x
+fun Sprite.getCenterY(): Float = this.originY + this.y
+fun Sprite.getCenter(): Vector2 = Vector2(this.getCenterX(), this.getCenterY())
 
 fun generateButton(texture: Texture): Button {
     return Button(Button.ButtonStyle().apply {
